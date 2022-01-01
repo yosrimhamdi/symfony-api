8531ce8badcddfdfd00cab1a355eff549cb16d09
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-01 16:42:25 +0100

    Generated the costumers and invoices fixtures.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,46 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Invoice;
+use App\Entity\Customer;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
-class AppFixtures extends Fixture
-{
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+class AppFixtures extends Fixture {
+    public function load(ObjectManager $manager): void {
+        $faker = \Faker\Factory::create();
+
+        for ($c = 0; $c < mt_rand(6, 10); $c++) {
+            $customer = new Customer();
+
+            $customer
+                ->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setCompany($faker->company);
+
+            $manager->persist($customer);
+
+            for ($i = 0; $i < mt_rand(2, 5); $i++) {
+                $invoice = new Invoice();
+
+                $invoice
+                    ->setCustomer($customer)
+                    ->setAmount($faker->randomFloat(2, 500, 1000))
+                    ->setSentAt(
+                        new \DateTimeImmutable(
+                            $faker
+                                ->dateTimeBetween('-6 months')
+                                ->format('Y-m-d H:i:s')
+                        )
+                    )
+                    ->setStatus(
+                        $faker->randomElement(['PAID', 'SENT', 'CANCELLED'])
+                    );
+
+                $manager->persist($invoice);
+            }
+        }
 
         $manager->flush();
     }
