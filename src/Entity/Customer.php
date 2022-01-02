59352389f65ff216a7122f0622ddc3afd6bfeb6a
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-02 19:31:12 +0100

    Filtering the requested data.

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -2,7 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\CustomerRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,6 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CustomerRepository::class)]
 #[ApiResource]
+#[ApiFilter(SearchFilter::class)]
 class Customer
 {
     #[ORM\Id]
