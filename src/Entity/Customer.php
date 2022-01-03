8b239b7ce562b4510f27fa6ac77db37e01eb4bc1
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-03 14:47:25 +0100

    Enabling GET /api/customers/{id}/invoices endpoint.

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\CustomerRepository;
@@ -49,6 +50,7 @@ class Customer
 
     #[ORM\OneToMany(mappedBy: 'customer', targetEntity: Invoice::class)]
     #[Groups(['customers_read'])]
+    #[ApiSubresource]
     private $invoices;
 
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'customers')]
