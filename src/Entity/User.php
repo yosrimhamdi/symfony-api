b170eb07fdec6b85621f37324c5a7571c69033b2
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-05 17:17:48 +0100

    users password no available in the response.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,24 +14,32 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ApiResource]
+#[
+    ApiResource(
+        normalizationContext: [
+            'groups' => ['users_read'],
+        ]
+    )
+]
+
 #[UniqueEntity('email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    #[Groups(['customers_read'])]
+    #[Groups(['customers_read', 'users_read'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 180, unique: true)]
-    #[Groups(['customers_read'])]
+    #[Groups(['customers_read', 'users_read'])]
     #[Assert\NotBlank]
     #[Assert\Length(max: 180)]
     #[Assert\Email]
     private $email;
 
     #[ORM\Column(type: 'json')]
+    #[Groups(['customers_read', 'users_read'])]
     private $roles = [];
 
     #[ORM\Column(type: 'string')]
@@ -40,13 +48,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $password;
 
     #[ORM\Column(type: 'string', length: 255)]
-    #[Groups(['customers_read'])]
+    #[Groups(['customers_read', 'users_read'])]
     #[Assert\NotBlank]
     #[Assert\Length(max: 255)]
     private $firstName;
 
     #[ORM\Column(type: 'string', length: 255)]
-    #[Groups(['customers_read'])]
+    #[Groups(['customers_read', 'users_read'])]
     #[Assert\NotBlank]
     #[Assert\Length(max: 255)]
     private $lastName;
