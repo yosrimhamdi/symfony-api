8ce49ae915edaabd1110b185cd902b0270094037
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-05 15:47:00 +0100

    Added the UniqueEntity email field.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,7 +15,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ApiResource]
-#[UniqueEntity]
+#[UniqueEntity('email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
