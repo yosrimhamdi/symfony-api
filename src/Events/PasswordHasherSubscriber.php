5cd203dd4ae3b1eb23b14b39e37fa2a429c9d382
yosrimhamdi <yosrimhamdi@outlook.com>
2022-01-06 21:59:25 +0100

    Setting the current logged in user to the newly created costumer.

--- src/Events/PasswordHasherSubscriber.php
+++ src/Events/PasswordHasherSubscriber.php
@@ -2,10 +2,10 @@
 
 namespace App\Events;
 
+use App\Entity\User;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use ApiPlatform\Core\EventListener\EventPriorities;
-use App\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
